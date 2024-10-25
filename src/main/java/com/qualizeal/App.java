199019a3c25ecf21e1b3e9451256461be27cb301
Mounika Sri <mounikasriavidi@gmail.com>
2024-10-25 10:50:49 +0530

    Added TwoNonDecimalNumbersMultiplication Files

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,12 @@
 package com.qualizeal;
-
+import com.qualizeal.javaapp04.operation.TwoNonDecimalNumbersMultiplication;
 public class App {
 	public static void main(String[] args) {
+		TwoNonDecimalNumbersMultiplication twoNonDecimalNumbersMultiplication = new TwoNonDecimalNumbersMultiplication();
+		long result = twoNonDecimalNumbersMultiplication.multiply(1,0);
+		System.out.println(result);
 		
 	}
 }
+
+
