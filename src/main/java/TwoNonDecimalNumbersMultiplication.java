4ac4ab75ee2f2d063b8415680eb6721af2b37f09
( sura deepika) <( suradeepika2515@gmail.com)>
2024-10-25 14:53:24 +0530

    solution

---
+++ src/main/java/TwoNonDecimalNumbersMultiplication.java
@@ -0,0 +1,17 @@
+package com.qualizeal.javaapp04.operation;
+ 
+public class TwoNonDecimalNumbersMultiplication{
+	public long multiply(int num1 , int num2){
+		int result = 0;
+		if (num1 < 0 || num2 < 0){
+			return -1;
+		}
+		else if (num1 == 0 || num2 == 0){
+			return -2;
+		}
+		else {
+			result = num1 * num2;
+		}
+		return result;
+	}
+}
