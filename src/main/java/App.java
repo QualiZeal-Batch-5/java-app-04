619ef1cb53234bbb278264b3ba00ce971b896488
Chandana Grandhi <grandhichandana4830@gmail.com>
2024-10-25 10:45:19 +0530

    addingfiles

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+package com.qualizeal.javaapp04.operation;
+
+import com.qualizeal.javaapp04.operation.TwoNonDecimalNumbersMultiplication;
+
+public class App{
+	public static void main(String[] args){
+		
+		TwoNonDecimalNumbersMultiplication twoNonDecimalNumbersMultiplication =  new TwoNonDecimalNumbersMultiplication();
+		
+		long result1 = twoNonDecimalNumbersMultiplication.multiply(10,2);
+		
+		System.out.println(result1);
+		
+	}
+}
