776e6eeb16a026f371f55b8b8e07b0be9ec2a46b
Disney Pallavi Bezawada <disneypallavi@gmail.com>
2024-10-25 10:31:51 +0530

    Added files.

---
+++ src/main/java/TwoNonDecimalNumbersMultiplication.java
@@ -0,0 +1,14 @@
+package com.qualizeal.javaapp04.operation;
+
+public class TwoNonDecimalNumbersMultiplication {
+	public long multiply(int num1,int num2) {
+		if(num1<0 || num2<0) {
+			return -1;
+		}	
+		if(num1==0 || num2==0) {
+			return -2;
+		}
+		
+        return num1*num2;	
+	}
+}
