2d8b53072683c8d96b7658078fc1f60032af1089
Sri Harika <nookalasriharika@gmail.com>
2024-10-25 10:57:55 +0530

    Multiplication

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,11 @@
 package com.qualizeal;
+import com.qualizeal.javaapp04.operation.TwoNonDecimalNumbersMultiplication;
 
 public class App {
 	public static void main(String[] args) {
+		TwoNonDecimalNumbersMultiplication twoNonDecimalNumbersMultiplication = new TwoNonDecimalNumbersMultiplication();
+		System.out.println(twoNonDecimalNumbersMultiplication.multiply(2,5));
+		System.out.println(twoNonDecimalNumbersMultiplication.multiply(8,5));
 		
 	}
 }
