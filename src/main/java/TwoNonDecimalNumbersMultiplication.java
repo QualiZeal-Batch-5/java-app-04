bba1a86a39eb4f4e2c8528e9116ba85a388583da
Shravani <sravanig066@gmail.com>
2024-10-25 11:42:46 +0530

    Multiplication solutions

---
+++ src/main/java/TwoNonDecimalNumbersMultiplication.java
@@ -0,0 +1,14 @@
+package com.qualizeal.javaapp04.operation;
+public class TwoNonDecimalNumbersMultiplication {
+public long multiply(int a,int b) {
+int result = 0;
+if(a<0 || b<0) {
+result = -1; }
+else if(a == 0 || b == 0) {
+result = -2;
+} else {
+result = a * b;
+}
+return result;
+}
+}
