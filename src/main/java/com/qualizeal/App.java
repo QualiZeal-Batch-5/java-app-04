3a62bf66f1a0b96fba0c0bf7cfd2ade9be909bec
vaditya kalyani <kalyanivaditya@gmail.com>
2024-10-25 10:49:53 +0530

    Solution added

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,17 @@
-package com.qualizeal;
+package com.qualizeal.javaapp04;
+import com.qualizeal.javaapp04.operation.TwoNonDecimalNumbersMultiplication;
 
 public class App {
 	public static void main(String[] args) {
-		
+       TwoNonDecimalNumbersMultiplication twoNonDecimalNumbersMultiplication = new TwoNonDecimalNumbersMultiplication();
+       long result1 =  twoNonDecimalNumbersMultiplication.multiply(200,10);
+       System.out.println(result1);
+       long result2 =  twoNonDecimalNumbersMultiplication.multiply(10,-3);
+       System.out.println(result2);
+       long result3 =  twoNonDecimalNumbersMultiplication.multiply(2,0);
+       System.out.println(result3);
+
+
+
 	}
 }
