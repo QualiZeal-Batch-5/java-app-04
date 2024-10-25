55f83b81a69395ef6b3ff5b8ca42581fe1e573df
Keerthi Priya <keerthipriya1463@gmail.com>
2024-10-25 10:44:06 +0530

    performed multiplication operation

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,11 @@
-package com.qualizeal;
-
+package com.qualizeal.javaapp04;
+ 
+import com.qualizeal.javaapp04.operation.TwoNonDecimalNumbersMultiplication;
+ 
 public class App {
 	public static void main(String[] args) {
-		
+		TwoNonDecimalNumbersMultiplication twoNonDecimalnumbersMultiplication = new TwoNonDecimalNumbersMultiplication();
+		long result = twoNonDecimalnumbersMultiplication.multiply(20,10);
+		System.out.println(result);
 	}
 }
