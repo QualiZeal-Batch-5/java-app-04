e178c6a62d454a15d4c81800758cdb8236dcb128
Madhusri <polamurimadhusri363@gmail.com>
2024-10-25 10:32:25 +0530

    Solution to TwoNonDecimalNumbersMultiplication

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,11 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp04.operation.TwoNonDecimalNumbersMultiplication;
+
 public class App {
 	public static void main(String[] args) {
-		
+		TwoNonDecimalNumbersMultiplication twoNonDecimalNumbersMultiplication = new TwoNonDecimalNumbersMultiplication();
+	    long result1 = twoNonDecimalNumbersMultiplication.multiply(40,20);
+		System.out.println(result1);
 	}
 }
