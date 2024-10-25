fd7c167eda3be0e1b1329b2d7c9eafcea14de5bf
Dandu Srija <dandusrija2503@gmail.com>
2024-10-25 11:01:45 +0530

    Performed multiplication operation on two non-decimal numbers.

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,15 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp04.operation.TwoNonDecimalNumbersMultiplication;
+
 public class App {
 	public static void main(String[] args) {
 		
+		TwoNonDecimalNumbersMultiplication TwoNonDecimalNumbersMultiplication = new TwoNonDecimalNumbersMultiplication();
+		long result = TwoNonDecimalNumbersMultiplication.multiply(10,2); 
+		System.out.println(result);
+		
 	}
 }
+
+
