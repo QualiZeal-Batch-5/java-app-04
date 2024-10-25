f028f9136b992b9fa8022a4076103e8f3a3bc88c
Ramya Mudragalla <mudragallaramya28@gmail.com>
2024-10-25 10:27:38 +0530

    Multiplication

---
+++ src/main/java/TwoNonDecimalNumbersMultiplication.java
@@ -0,0 +1,16 @@
+package com.qualizeal.javaapp04.operation;
+public class TwoNonDecimalNumbersMultiplication {
+	public long multiply(int num1, int num2){
+		long result = 0;
+		if (num1 <0 || num2 < 0){
+			result = -1;
+		} else if(num1 == 0 || num2 ==0){
+			result = -2;
+		}
+		else{
+			result  = (long) num1 * num2;
+		}
+		return result;
+	}
+}
+	
