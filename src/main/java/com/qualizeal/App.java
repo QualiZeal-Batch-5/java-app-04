ab30458f2b9bf4fc768d68c0950e83c76dfce2c8
vijaya lakshmi <dasarisaibalaji2006@gmail.com>
2024-10-25 10:17:24 +0530

    Solution added

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,11 @@
 package com.qualizeal;
+import com.qualizeal.javaapp04.operation.TwoNonDecimalNumbersMultiplication;
 
 public class App {
 	public static void main(String[] args) {
+		TwoNonDecimalNumbersMultiplication twoNonDecimalNumbersMultiplication = new TwoNonDecimalNumbersMultiplication();
+		long result = twoNonDecimalNumbersMultiplication.multiply(10,2);
+		System.out.println(result);
 		
 	}
 }
