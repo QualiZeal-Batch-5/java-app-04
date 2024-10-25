30e528440e8a5659084146804146081e9d9f03f5
Swarna Modepalli <swarnamodepalli954@gmail.com>
2024-10-25 10:50:31 +0530

    Solution added.

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,10 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp04.operation.TwoNonDecimalNumbersMultiplication;
 public class App {
 	public static void main(String[] args) {
-		
+		TwoNonDecimalNumbersMultiplication twoNonDecimalNumbersMultiplication = new TwoNonDecimalNumbersMultiplication();
+		double result1 = twoNonDecimalNumbersMultiplication.multiply(4,2);
+		System.out.println(result1);
 	}
 }
